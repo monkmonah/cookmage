b26b712db9c26fc12d335f619eeef74b6c022d39
Vandermar <hilko.andrey@gmail.com>
2017-06-07 14:09:27 +0300

    Hello

---
+++ src/main/java/ml/cookmage/CookmageApplication.java
@@ -0,0 +1,12 @@
+package ml.cookmage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookmageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookmageApplication.class, args);
+	}
+}
---
+++ src/main/java/ml/cookmage/HelloController.java
@@ -0,0 +1,12 @@
+package ml.cookmage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World";
+    }
+}
