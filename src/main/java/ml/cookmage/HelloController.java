a42f1a37cf4653f4733f7dbc3ebd1e6ad71207e0
Vandermar <hilko.andrey@gmail.com>
2017-06-08 09:48:46 +0300

    Greeting with param added

--- src/main/java/ml/cookmage/HelloController.java
+++ src/main/java/ml/cookmage/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping("/hello")
+    public String index() {
         return "Hello World";
     }
 }
